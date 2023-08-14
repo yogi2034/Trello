cc9256307f9fd6cbc8fd4b3a6ecd971730245b09
yogitha  seelam <yogilss02@gmail.com>
2023-08-14 07:43:25 +0000

    modified main page

--- trello-appwrite-clone/app/page.tsx
+++ trello-appwrite-clone/app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Trello 2.0</h1>    </main>
+    <main >
+      <h1>Trello 2.0</h1>  
+        </main>
   )
 }
