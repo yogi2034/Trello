948e705a1305e5f3fe4ef0b19021b4e9df0307ab
yogitha  seelam <yogilss02@gmail.com>
2023-08-15 16:03:30 +0000

    header UI changed

--- trello-appwrite-clone/components/Header.tsx
+++ trello-appwrite-clone/components/Header.tsx
@@ -1,7 +1,9 @@
+'use client'
 import Image from 'next/image'
-import { MagnifyingGlassIcon,UserCircleIcon} from '@heroicons/react/24/solid'
+import { CircleStackIcon, MagnifyingGlassIcon,UserCircleIcon} from '@heroicons/react/24/solid'
 
 import React from 'react'
+import Avatar from 'react-avatar'
 
 function Header() {
   return (
@@ -10,11 +12,11 @@ function Header() {
         <Image
         src="https://links.papareact.com/c2cdd5"
         alt ="trello logo"
-        width={200}
-        height={100}
+        width={150}
+        height={75}
         className='w-44 md:w-56 pb-0 object-contain'
         />
-        <div>
+        <div className='flex items-center space-x-5 flex-1 justify-end w-full'>
             {/*search box*/}
             <form className='flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial'>
                 <MagnifyingGlassIcon className='h-6 w-6 text-gray-400'/>
@@ -22,7 +24,15 @@ function Header() {
                 <input type="text" placeholder='Search' className='flex-1 outline-none p-2'/>
                 <button  type='submit'hidden>Search</button>
             </form>
+            {/*Avatar*/}
+            <Avatar name='yogi' round size='50' color='#0055D1' className='flex'/>
         </div>
+        </div>
+        <div className='flex items-center justify-center px-5 md:py-5'>
+          <p className='flex items-center p-2 text-sm font-light pr-5  shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#5500D1]'>
+          <UserCircleIcon className='inline-block h-10 w-10 text-[#0055D1] mr-1'/>
+          GPT is summarizing your tasks..
+          </p>
         </div>
     </header>
   )
