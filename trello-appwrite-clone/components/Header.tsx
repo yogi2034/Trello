98630f35ed1a7d71d12f91800b02ad0f71183fcf
yogitha  seelam <yogilss02@gmail.com>
2023-08-15 16:27:27 +0000

    Gradient add

--- trello-appwrite-clone/components/Header.tsx
+++ trello-appwrite-clone/components/Header.tsx
@@ -9,6 +9,22 @@ function Header() {
   return (
     <header>
         <div className='flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl'>
+          <div
+          className="
+          absolute
+          top-0
+          w-full
+          h-96
+          bg-gradient-to-br
+          from-pink-400
+          to-[#5500D1]
+          rounded-md
+          filter
+          blur-3xl
+          opacity-50
+          -z-50"
+          />
+
         <Image
         src="https://links.papareact.com/c2cdd5"
         alt ="trello logo"
